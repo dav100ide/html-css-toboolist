955d374cfc1c1293786fbff14b89883078abc32c
dav100ide <oreglio.dev@gmail.com>
2022-09-13 17:27:21 +0200

    cazzeggio JS

---
+++ cazzeggio.js
@@ -0,0 +1,7 @@
+function nonCapace() {
+  document.getElementById('non-capace').innerHTML = 'ERRORE non sono ancora capace a creare una pagina interattiva😢';
+}
+
+function togli() {
+  document.getElementById('non-capace').innerHTML = '';
+}
